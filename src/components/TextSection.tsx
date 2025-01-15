a0d62a5ffddd6ec2529b1c6206eb787396ed38b6
Felipe Torres <felipe.torressepulveda@gmail.com>
2025-01-15 12:41:34 -0800

    update

--- src/components/TextSection.tsx
+++ src/components/TextSection.tsx
@@ -3,14 +3,14 @@ export const TextSection = () => {
     <section className="flex flex-col bg-white py-28" id="next-section">
       <div className="container mx-auto px-11">
         <p className="leading-10 lg:leading-relaxed max-w-5xl mx-auto text-4xl lg:text-4xl tracking-tight">
-          <strong>JSConf Chile está de vuelta!</strong> Durante casi una década,
-          hemos construido una comunidad vibrante enfocada en{" "}
+          <strong>JSConf Chile vuelve con todo!</strong> Durante casi una
+          década, hemos construido una comunidad vibrante enfocada en{" "}
           <strong>tecnología, desarrollo, y JavaScript.</strong>
           <br />
           <br />
-          Este 2025 volvemos con más fuerza y tenemos grandes planes! Y para
-          hacer esto posible, hemos creado una colección exclusiva de productos
-          JSConf Chile que puedes adquirir, y ayudar a financiar la JSConf e
+          Este 2025 tenemos más fuerza y grandes planes! Para hacer todo esto
+          posible, hemos creado una colección exclusiva de productos JSConf
+          Chile que puedes adquirir, y ayudar a financiar la JSConf e
           iniciativas que apoyan a mas de 5000 personas a lo largo de Chile.
           <br />
           <br />
